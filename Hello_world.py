a0e3cec0795fdf6c8b4b711e197a69ec77dc38b3
Bartosz Andrzejewski <Arkand95@gmail.com>
2019-01-09 17:06:11 +0000

    first commit

---
+++ Hello_world.py
@@ -0,0 +1,6 @@
+print("-----------------------------")
+print("       HELLO WORLD")
+print("-----------------------------")
+
+name = input("What is your name? ")
+print("Nice to meet you " + name + "!")
